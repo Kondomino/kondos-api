77ed345d96941538fb566aae022de397e56eac2e
Victor Souto <souto.victor@gmail.com>
2024-03-13 20:09:06 -0300

    migrations on root

---
+++ src/user/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { userProviders } from './user.provider';
+
+@Module({
+  controllers: [UserController],
+  providers: [UserService, ...userProviders]
+})
+export class UserModule {}
