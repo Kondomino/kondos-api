8e1143a9c2003631d659dbfc1aa09836293f0e05
Victor Souto <souto.victor@gmail.com>
2024-06-13 11:38:52 -0300

    media seeder improved and starting to register seeds

--- src/media/seeds/media.seeds.ts
+++ src/media/seeds/media.seeds.ts
@@ -1,5 +1,43 @@
 
 export const mediaSeeds = [
+   
+   //  98	la-ville
+   {
+      filename: 'la-ville-1',
+      kondoId: 98
+   },
+   {
+      filename: 'la-ville-2',
+      kondoId: 98
+   },
+   {
+      filename: 'la-ville-3',
+      kondoId: 98
+   },
+   {
+      filename: 'la-ville-4',
+      kondoId: 98
+   },
+   {
+      filename: 'la-ville-5',
+      kondoId: 98
+   },
+   {
+      filename: 'la-ville-',
+      kondoId: 98,
+      type: 'video'
+   },
+   {
+      filename: 'la-ville-',
+      kondoId: 98,
+      type: 'video'
+   },
+   {
+      filename: 'la-ville-',
+      kondoId: 98,
+      type: 'video'
+   },
+   //  99	fazendas-terras-de-minas
     {
        filename: 'mirrage-nautic-2',
        kondoId: 60
@@ -10,102 +48,111 @@ export const mediaSeeds = [
     },
  ];
 
-
-// 1	vitoria-nautico-residencevitoria-da-uniao
-// 2	vale-dos-sonhos
-// 3	victoria-golf-residencevitoria-da-uniao
-// 4	village-do-gramado
-// 5	encanto-da-lagoa
-// 6	champagne
-// 7	campos-da-liberdade
-// 8	sonho-verde
-// 9	canto-da-siriema
-// 10	vista-da-lagoa
-// 11	prime
-// 12	igarape
-// 13	nautico
-// 14	represa-ville
-// 15	parque-sao-jose
-// 16	estancias-de-glaura
-// 17	ouro-de-minas
-// 18	residencial-reserva-da-barra
-// 19	capela-nova
-// 20	bela-vida
-// 21	arvoredo-do-tripui
-// 22	sao-damiao
-// 23	bairro-residencial-visao
-// 24	recanto-verde
-// 25	estancia-da-cachoeira
-// 26	aldeia-da-cachoeira-das-pedras
-// 27	parque-da-lagoa
-// 28	portal-do-igarape
-// 29	dona-quita
-// 30	viva-boulevard
-// 31	vila-da-serra
-// 32	estrela-da-mata-ville
-// 33	ville-jardins
-// 34	green-ville-capelinha
-// 35	morro-dos-ventos
-// 36	recanto-da-floresta
-// 37	house-club
-// 38	nautico-ilha-brasil
-// 39	vale-do-solsete-lagoas
-// 40	mirrage-nautico
-// 41	serra-do-cipo
-// 42	jequitiba
-// 43	ville-des-lacs
-// 44	veradas-das-gerais
-// 45	arvoredogreen-park
-// 46	jardins-de-petropolis
-// 47	miguelao
-// 48	lagoa-do-miguelao
-// 49	ville-de-montagne
-// 50	pasargada
-// 51	parque-do-engenho
-// 52	ouro-velo-mansoes
-// 53	vila-do-ouro
-// 54	estancia-serrana
-// 55	condominio-das-flores-alphaville
-// 56	reserva-laguna
-// 57	veradas-das-geraes
-// 58	green-park
-// 59	arvoredo
-// 60	vale-da-mata
-// 61	reserva-aguas-claras
-// 62	alphaville
-// 63	bosque-da-ribeira
-// 64	estancia-da-serra
-// 65	morro-do-chapeu-golf-club
-// 66	quintas-do-morro
-// 67	quintas-do-sol
-// 68	riviera
-// 69	conde
-// 70	serra-dos-manacas
-// 71	vale-dos-cristais
-// 72	estancia-alpina
-// 73	estancia-do-estoril-i
-// 74	estancia-do-estoril-ii
-// 75	vila-alpina
-// 76	vila-castela
-// 77	vila-del-rey
-// 78	village-terrasse
-// 79	quintas-da-cachoeira
-// 80	alphaville-alameda-das-aguas
-// 81	boavista-residencial
-// 82	quinta-da-villa
-// 83	fazendinhas-aguas-do-cerrado
-// 84	gran-royale
-// 85	reservas-do-lagochapeu-d-uvas
-// 86	tres-valesresidencia-esporte-natureza
-// 87	ecoville-iii
-// 88	eco-village
-// 89	terra-vista
-// 90	canto-da-siriemaresort-residence
-// 91	serra-morena
-// 92	fazendinhas-eldorado
-// 93	caminho-das-cachoeiras
-// 94	serras-altas-residencial
-// 95	paradiso-ecologico
-// 96	estancia-do-lago
-// 97	balneario-vista-da-lagoa
-// 98  la-ville
+//  1	vitoria-nautico-residencevitoria-da-uniao
+//  2	victoria-golf-residencevitoria-da-uniao
+//  3	village-do-gramado
+//  4	vale-dos-sonhos
+//  5	champagne
+//  6	encanto-da-lagoa
+//  7	campos-da-liberdade
+//  8	sonho-verde
+//  9	canto-da-siriema
+//  10	vista-da-lagoa
+//  11	prime
+//  12	igarape
+//  13	nautico
+//  14	represa-ville
+//  15	parque-sao-jose
+//  16	estancias-de-glaura
+//  17	ouro-de-minas
+//  18	residencial-reserva-da-barra
+//  19	capela-nova
+//  20	bela-vida
+//  21	arvoredo-do-tripui
+//  22	sao-damiao
+//  23	bairro-residencial-visao
+//  24	recanto-verde
+//  25	estancia-da-cachoeira
+//  26	aldeia-da-cachoeira-das-pedras
+//  27	parque-da-lagoa
+//  28	portal-do-igarape
+//  29	dona-quita
+//  30	viva-boulevard
+//  31	vila-da-serra
+//  32	estrela-da-mata-ville
+//  33	ville-jardins
+//  34	green-ville-capelinha
+//  35	morro-dos-ventos
+//  36	recanto-da-floresta
+//  37	house-club
+//  38	nautico-ilha-brasil
+//  39	vale-do-solsete-lagoas
+//  40	mirrage-nautico
+//  41	serra-do-cipo
+//  42	jequitiba
+//  43	ville-des-lacs
+//  44	veradas-das-gerais
+//  45	arvoredogreen-park
+//  46	jardins-de-petropolis
+//  47	miguelao
+//  48	lagoa-do-miguelao
+//  49	ville-de-montagne
+//  50	pasargada
+//  51	parque-do-engenho
+//  52	ouro-velo-mansoes
+//  53	vila-do-ouro
+//  54	estancia-serrana
+//  55	condominio-das-flores-alphaville
+//  56	reserva-laguna
+//  57	veradas-das-geraes
+//  58	green-park
+//  59	arvoredo
+//  60	vale-da-mata
+//  61	reserva-aguas-claras
+//  62	alphaville
+//  63	bosque-da-ribeira
+//  64	estancia-da-serra
+//  65	morro-do-chapeu-golf-club
+//  66	quintas-do-morro
+//  67	quintas-do-sol
+//  68	riviera
+//  69	conde
+//  70	serra-dos-manacas
+//  71	vale-dos-cristais
+//  72	estancia-alpina
+//  73	estancia-do-estoril-i
+//  74	estancia-do-estoril-ii
+//  75	vila-alpina
+//  76	
+//  77	vila-del-rey
+//  78	village-terrasse
+//  79	quintas-da-cachoeira
+//  80	alphaville-alameda-das-aguas
+//  81	boavista-residencial
+//  82	quinta-da-villa
+//  83	fazendinhas-aguas-do-cerrado
+//  84	gran-royale
+//  85	reservas-do-lagochapeu-d-uvas
+//  86	tres-valesresidencia-esporte-natureza
+//  87	ecoville-iii
+//  88	eco-village
+//  89	terra-vista
+//  90	canto-da-siriemaresort-residence
+//  91	serra-morena
+//  92	fazendinhas-eldorado
+//  93	caminho-das-cachoeiras
+//  94	serras-altas-residencial
+//  95	paradiso-ecologico
+//  96	estancia-do-lago
+//  97	balneario-vista-da-lagoa
+//  98	la-ville
+//  99	fazendas-terras-de-minas
+//  100	vila-castela-brisas
+//  101	vila-castela-jardins
+//  102	vila-castela-alameda
+//  103	vila-castela-bougainville
+//  104	vila-castela-solares
+//  105	vila-castela-real
+//  106	vila-castela-arvoredo
+//  107	vila-castela-ipe
+//  108	vila-castela-reserva
