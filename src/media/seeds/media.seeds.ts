50688eea0ec68f2ef5c3e49b99433953b37337ed
Victor Souto <souto.victor@gmail.com>
2024-06-17 08:27:40 -0300

    implementing new seeds for Medias

--- src/media/seeds/media.seeds.ts
+++ src/media/seeds/media.seeds.ts
@@ -38,114 +38,444 @@ export const mediaSeeds = [
       type: 'video'
    },
    //  99	fazendas-terras-de-minas
-    
- ];
+   {
+      filename: 'terras-de-minas-0.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-0b.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-0c.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-0d.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-0e.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-2.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-3.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-4.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-5.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-6.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-7.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-8.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-9.jpeg',
+      kondoId: 99
+   },
+   {
+      filename: 'terras-de-minas-10.jpeg',
+      kondoId: 99
+   },
+   // video
+   {
+      filename: 'terras-de-minas-1.mp4',
+      kondoId: 99,
+      type: 'video'
+   },
+   // 115	estancia-da-lapa
+   {
+      filename: 'estancia-da-lapa-1.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-2.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-3.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-4.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-5.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-6.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-7.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-8.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-9.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-10.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-11.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-12.png',
+      kondoId: 115
+   },
+   {
+      filename: 'estancia-da-lapa-13.png',
+      kondoId: 115
+   },
+   // video
+   {
+      filename: 'estancia-da-lapa-13.mp4',
+      kondoId: 115,
+      type: 'video'
+   },
+   // 113	gandara
+   {
+      filename: 'gandara-1.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-2.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-3.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-4.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-5.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-6.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-7.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-8.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-9.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-10.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-11.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-12.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-13.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-14.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-15.jpeg',
+      kondoId: 113
+   },
+   {
+      filename: 'gandara-16.jpeg',
+      kondoId: 113
+   },
+   // video
+   {
+      filename: 'gandara-1.mp4',
+      kondoId: 113,
+      type: 'video'
+   },
+   {
+      filename: 'gandara-2.mp4',
+      kondoId: 113,
+      type: 'video'
+   }, 
+   // 112	versailles
+   {
+      filename: 'versailles-1.jpeg',
+      kondoId: 112
+   },
+   {
+      filename: 'versailles-2.jpeg',
+      kondoId: 112
+   },
+   {
+      filename: 'versailles-3.jpeg',
+      kondoId: 112
+   },
+   {
+      filename: 'versailles-4.jpeg',
+      kondoId: 112
+   },
+   {
+      filename: 'versailles-5.jpeg',
+      kondoId: 112
+   },
+   {
+      filename: 'versailles-6.jpeg',
+      kondoId: 112
+   },
+   {
+      filename: 'versailles-7.jpeg',
+      kondoId: 112
+   },
+   {
+      filename: 'versailles-8.jpeg',
+      kondoId: 112
+   },
+   {
+      filename: 'versailles-9.jpeg',
+      kondoId: 112
+   },
+   // video
+   {
+      filename: 'versailles-1.mp4',
+      kondoId: 112,
+      type: 'video'
+   }, 
+   // 111	mae-terra
+   {
+      filename: 'mae-terra-1.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-2.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-3.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-4.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-5.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-6.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-7.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-8.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-9.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-10.jpg',
+      kondoId: 111
+   },
+   {
+      filename: 'mae-terra-11.jpg',
+      kondoId: 111
+   },
+   // video
+   {
+      filename: 'versailles-1.mp4',
+      kondoId: 111,
+      type: 'video'
+   }, 
+   // 110	aguas-do-funil
+   {
+      filename: 'aguas-do-funil-1.jpeg',
+      kondoId: 110
+   },
+   {
+      filename: 'aguas-do-funil-2.jpeg',
+      kondoId: 110
+   },
+   {
+      filename: 'aguas-do-funil-3.jpeg',
+      kondoId: 110
+   },
+   {
+      filename: 'aguas-do-funil-4.jpeg',
+      kondoId: 110
+   },
+   {
+      filename: 'aguas-do-funil-5.jpeg',
+      kondoId: 110
+   },
+   {
+      filename: 'aguas-do-funil-6.jpeg',
+      kondoId: 110
+   },
+   {
+      filename: 'aguas-do-funil-7.jpeg',
+      kondoId: 110
+   },
+   {
+      filename: 'aguas-do-funil-8.jpeg',
+      kondoId: 110
+   },
+   // video
+   {
+      filename: 'versailles-1.mp4',
+      kondoId: 110,
+      type: 'video'
+   }, 
+];
 
 //  1	vitoria-nautico-residencevitoria-da-uniao
-//  2	victoria-golf-residencevitoria-da-uniao
-//  3	village-do-gramado
-//  4	vale-dos-sonhos
-//  5	champagne
-//  6	encanto-da-lagoa
-//  7	campos-da-liberdade
-//  8	sonho-verde
-//  9	canto-da-siriema
-//  10	vista-da-lagoa
-//  11	prime
-//  12	igarape
-//  13	nautico
-//  14	represa-ville
-//  15	parque-sao-jose
-//  16	estancias-de-glaura
-//  17	ouro-de-minas
-//  18	residencial-reserva-da-barra
-//  19	capela-nova
-//  20	bela-vida
-//  21	arvoredo-do-tripui
-//  22	sao-damiao
-//  23	bairro-residencial-visao
-//  24	recanto-verde
-//  25	estancia-da-cachoeira
-//  26	aldeia-da-cachoeira-das-pedras
-//  27	parque-da-lagoa
-//  28	portal-do-igarape
-//  29	dona-quita
-//  30	viva-boulevard
-//  31	vila-da-serra
-//  32	estrela-da-mata-ville
-//  33	ville-jardins
-//  34	green-ville-capelinha
-//  35	morro-dos-ventos
-//  36	recanto-da-floresta
-//  37	house-club
-//  38	nautico-ilha-brasil
-//  39	vale-do-solsete-lagoas
-//  40	mirrage-nautico
-//  41	serra-do-cipo
-//  42	jequitiba
-//  43	ville-des-lacs
-//  44	veradas-das-gerais
-//  45	arvoredogreen-park
-//  46	jardins-de-petropolis
-//  47	miguelao
-//  48	lagoa-do-miguelao
-//  49	ville-de-montagne
-//  50	pasargada
-//  51	parque-do-engenho
-//  52	ouro-velo-mansoes
-//  53	vila-do-ouro
-//  54	estancia-serrana
-//  55	condominio-das-flores-alphaville
-//  56	reserva-laguna
-//  57	veradas-das-geraes
-//  58	green-park
-//  59	arvoredo
-//  60	vale-da-mata
-//  61	reserva-aguas-claras
-//  62	alphaville
-//  63	bosque-da-ribeira
-//  64	estancia-da-serra
-//  65	morro-do-chapeu-golf-club
-//  66	quintas-do-morro
-//  67	quintas-do-sol
-//  68	riviera
-//  69	conde
-//  70	serra-dos-manacas
-//  71	vale-dos-cristais
-//  72	estancia-alpina
-//  73	estancia-do-estoril-i
-//  74	estancia-do-estoril-ii
-//  75	vila-alpina
-//  76	
-//  77	vila-del-rey
-//  78	village-terrasse
-//  79	quintas-da-cachoeira
-//  80	alphaville-alameda-das-aguas
-//  81	boavista-residencial
-//  82	quinta-da-villa
-//  83	fazendinhas-aguas-do-cerrado
-//  84	gran-royale
-//  85	reservas-do-lagochapeu-d-uvas
-//  86	tres-valesresidencia-esporte-natureza
-//  87	ecoville-iii
-//  88	eco-village
-//  89	terra-vista
-//  90	canto-da-siriemaresort-residence
-//  91	serra-morena
-//  92	fazendinhas-eldorado
-//  93	caminho-das-cachoeiras
-//  94	serras-altas-residencial
-//  95	paradiso-ecologico
-//  96	estancia-do-lago
-//  97	balneario-vista-da-lagoa
-//  98	la-ville
-//  99	fazendas-terras-de-minas
-//  100	vila-castela-brisas
-//  101	vila-castela-jardins
-//  102	vila-castela-alameda
-//  103	vila-castela-bougainville
-//  104	vila-castela-solares
-//  105	vila-castela-real
-//  106	vila-castela-arvoredo
-//  107	vila-castela-ipe
-//  108	vila-castela-reserva
+// 2	victoria-golf-residencevitoria-da-uniao
+// 3	village-do-gramado
+// 4	vale-dos-sonhos
+// 5	champagne
+// 6	encanto-da-lagoa
+// 7	campos-da-liberdade
+// 8	sonho-verde
+// 9	canto-da-siriema
+// 10	vista-da-lagoa
+// 11	prime
+// 12	igarape
+// 13	nautico
+// 14	represa-ville
+// 15	parque-sao-jose
+// 16	estancias-de-glaura
+// 17	ouro-de-minas
+// 18	residencial-reserva-da-barra
+// 19	capela-nova
+// 20	bela-vida
+// 21	arvoredo-do-tripui
+// 22	sao-damiao
+// 23	bairro-residencial-visao
+// 24	recanto-verde
+// 25	estancia-da-cachoeira
+// 26	aldeia-da-cachoeira-das-pedras
+// 27	parque-da-lagoa
+// 28	portal-do-igarape
+// 29	dona-quita
+// 30	viva-boulevard
+// 31	vila-da-serra
+// 32	estrela-da-mata-ville
+// 33	ville-jardins
+// 34	green-ville-capelinha
+// 35	morro-dos-ventos
+// 36	recanto-da-floresta
+// 37	house-club
+// 38	nautico-ilha-brasil
+// 39	vale-do-solsete-lagoas
+// 40	mirrage-nautico
+// 41	serra-do-cipo
+// 42	jequitiba
+// 43	ville-des-lacs
+// 44	veradas-das-gerais
+// 45	arvoredogreen-park
+// 46	jardins-de-petropolis
+// 47	miguelao
+// 48	lagoa-do-miguelao
+// 49	ville-de-montagne
+// 50	pasargada
+// 51	parque-do-engenho
+// 52	ouro-velo-mansoes
+// 53	vila-do-ouro
+// 54	estancia-serrana
+// 55	condominio-das-flores-alphaville
+// 56	reserva-laguna
+// 57	veradas-das-geraes
+// 58	green-park
+// 59	arvoredo
+// 60	vale-da-mata
+// 61	reserva-aguas-claras
+// 62	alphaville
+// 63	bosque-da-ribeira
+// 64	estancia-da-serra
+// 65	morro-do-chapeu-golf-club
+// 66	quintas-do-morro
+// 67	quintas-do-sol
+// 68	riviera
+// 69	conde
+// 70	serra-dos-manacas
+// 71	vale-dos-cristais
+// 72	estancia-alpina
+// 73	estancia-do-estoril-i
+// 74	estancia-do-estoril-ii
+// 75	vila-alpina
+// 76	
+// 77	vila-del-rey
+// 78	village-terrasse
+// 79	quintas-da-cachoeira
+// 80	alphaville-alameda-das-aguas
+// 81	boavista-residencial
+// 82	quinta-da-villa
+// 83	fazendinhas-aguas-do-cerrado
+// 84	gran-royale
+// 85	reservas-do-lagochapeu-d-uvas
+// 86	tres-valesresidencia-esporte-natureza
+// 87	ecoville-iii
+// 88	eco-village
+// 89	terra-vista
+// 90	canto-da-siriemaresort-residence
+// 91	serra-morena
+// 92	fazendinhas-eldorado
+// 93	caminho-das-cachoeiras
+// 94	serras-altas-residencial
+// 95	paradiso-ecologico
+// 96	estancia-do-lago
+// 97	balneario-vista-da-lagoa
+// 98	la-ville
+// 99	fazendas-terras-de-minas
+// 100	vila-castela-brisas
+// 101	vila-castela-jardins
+// 102	vila-castela-alameda
+// 103	vila-castela-bougainville
+// 104	vila-castela-solares
+// 105	vila-castela-real
+// 106	vila-castela-arvoredo
+// 107	vila-castela-ipe
+// 108	vila-castela-reserva
+// 109	alto-dos-pinheiros
