b017b3aede6ab75ca9ab999fdeac76483c21a6ec
Victor Souto <souto.victor@gmail.com>
2024-06-17 15:46:12 -0300

    rebuilded

--- dist/kondo/repository/kondo.provider.js
+++ dist/kondo/repository/kondo.provider.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.kondoProviders = void 0;
 const constants_1 = require("../../core/constants");
-const kondo_entity_1 = require("../entities/kondo.entity");
+const Kondo_entity_1 = require("../entities/Kondo.entity");
 exports.kondoProviders = [{
         provide: constants_1.KONDO_REPOSITORY_PROVIDER,
-        useValue: kondo_entity_1.Kondo,
+        useValue: Kondo_entity_1.Kondo,
     }];
