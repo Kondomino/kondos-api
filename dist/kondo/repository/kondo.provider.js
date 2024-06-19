072981c41428c5fa85c8a764ebeb3cb2eb3f491a
Victor Souto <souto.victor@gmail.com>
2024-06-19 20:04:38 -0300

    pushing build with Kondo typo

--- dist/kondo/repository/kondo.provider.js
+++ dist/kondo/repository/kondo.provider.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.kondoProviders = void 0;
 const constants_1 = require("../../core/constants");
-const Kondo_entity_1 = require("../entities/Kondo.entity");
+const kondo_entity_1 = require("../entities/kondo.entity");
 exports.kondoProviders = [{
         provide: constants_1.KONDO_REPOSITORY_PROVIDER,
-        useValue: Kondo_entity_1.Kondo,
+        useValue: kondo_entity_1.Kondo,
     }];
