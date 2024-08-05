d2cea52488122efe0198b6f6c97ea2cea44011b1
Victor Souto <souto.victor@gmail.com>
2024-08-05 13:29:40 -0300

    tests

--- dist/media/media.controller.js
+++ dist/media/media.controller.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.MediaController = void 0;
 const common_1 = require("@nestjs/common");
 const media_service_1 = require("./media.service");
+const public_decorator_1 = require("../auth/decorators/public.decorator");
 let MediaController = class MediaController {
     constructor(mediaService) {
         this.mediaService = mediaService;
@@ -30,6 +31,7 @@ let MediaController = class MediaController {
     }
 };
 __decorate([
+    (0, public_decorator_1.Public)(),
     (0, common_1.Get)(':id'),
     __param(0, (0, common_1.Param)('id')),
     __metadata("design:type", Function),
